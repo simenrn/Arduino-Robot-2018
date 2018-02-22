dad3a2b2749542bda431089df04137d6f18b896f
Unknown <sironilssen@gmail.com>
2018-02-22 17:48:11 +0100

    Oppdatert de nye portkoblingene
    Lagt til de nye portene brukt til motorkontroller, servo og nRF

--- defines.h
+++ defines.h
@@ -23,25 +23,34 @@
 /************************************************************************/
 /* PHYSICAL CONSTANTS - If the robot is changed these need to be changed 
  Some of these will be sent to server during the start-up-handshake   
- Wheel factor is the circumference divided by ticks per rotation 
- Cog wheels: 40/16 and 40/8
- -> ((pi*81.6)/(198 * 40/16 * 40/8)) / 2 = 0.2072 mm 
-                        Length the robot travels per divided tick
-                        (Ticks are divided in motor.c)                  */
-#define WHEELBASE_MM             215.0 /* Length between wheel centers  */
-#define ROBOT_TOTAL_WIDTH_MM     210 /* From outer rim to outer rim   */
-#define ROBOT_TOTAL_LENGTH_MM    220 /* From front to aft, total      */
-#define ROBOT_AXEL_OFFSET_MM     39  /* From center of square         */
-#define SENSOR_TOWER_OFFSET_X_MM 30  /* From center of square         */ 
+ */
+
+#define WHEELBASE_MM             147.0 /* Length between wheel centers  */
+#define ROBOT_TOTAL_WIDTH_MM     180 /* From outer rim to outer rim   */
+#define ROBOT_TOTAL_LENGTH_MM    245 /* From front to aft, total      */
+#define ROBOT_AXEL_OFFSET_MM     56  /* From center of square         */
+#define SENSOR_TOWER_OFFSET_X_MM 56  /* From center of square         */ 
 #define SENSOR_TOWER_OFFSET_Y_MM 0   /* From center of square         */
-#define SENSOR_OFFSET_RADIUS_MM  22  /* From center of tower          */
+#define SENSOR_OFFSET_RADIUS_MM  21  /* From center of tower          */
 #define ROBOT_DEADLINE_MS        200 /* Interval between measurements */
 #define SENSOR1_HEADING_DEG      0   /* Sensor angle relative to body */
 #define SENSOR2_HEADING_DEG      90
 #define SENSOR3_HEADING_DEG      180
 #define SENSOR4_HEADING_DEG      270
 
-#define WHEEL_FACTOR_MM 0.2072          /* Calculated, see above */ 
+#define WHEEL_FACTOR_MM 0.638          /* Calculated, see below */ 
+
+/*
+Wheel factor is the circumference of the wheel divided by ticks per 
+rotation. Motors have a gear ratio of 48:1, and the encoder is attached
+to not geared down shaft with a 8 pole magnet. This means for every full
+rotation of the wheel, the encoder magnet will have 48 full rotations
+with 8 tick per rotation.
+
+-> (2*pi*39mm)/(48/1*8) = 0.6380 mm/tick - Length the robot travels per
+										   divided tick
+
+(Ticks are divided in motor.c)              						   */
 
 /************************************************************************/
 /* Program settings                                                     */
@@ -64,21 +73,21 @@
 /* Distance sensor                                                      */
 #define distSensPort    PORTF   /* Sensors connected to port A          */
 #define distSensReg     DDRF    /* Port A register used                 */
-#define distSensLeft    PINF0   /* Forward sensor connected to PA4      */
-#define distSensRear    PINF3   /* Backward sensor connected to PA3     */
-#define distSensRight   PINF2   /* Right sensor connected to PA2        */
-#define distSensFwd     PINF1   /* Left sensor connected to PA3         */
+#define distSensLeft    PINF1   /* Left sensor connected to PF1      */
+#define distSensRear    PINF3   /* Backward sensor connected to PF3     */
+#define distSensRight   PINF2   /* Right sensor connected to PF2        */
+#define distSensFwd     PINF0   /* Forward sensor connected to PF0         */
 
 /* Motor control                                                        */
-#define motorRightOn        PINB7
-#define motorRightPWM       OCR0A
-#define motorRightForward   PINA2   /* Right motor, pin 1               */
-#define motorRightBackward  PINA4   /* Right motor, pin 2               */
+#define motorRightOn        PINB6
+#define motorRightPWM       OCR1B
+#define motorRightForward   PINH6   /* Right motor, pin 1               */
+#define motorRightBackward  PINH5   /* Right motor, pin 2               */
 
-#define motorLeftOn         PING5
-#define motorLeftPWM        OCR0B
-#define motorLeftBackward   PINC7   /* Left motor, pin 1                */
-#define motorLeftForward    PINC5   /* Left motor, pin 2                */
+#define motorLeftOn         PINB7
+#define motorLeftPWM        OCR0A
+#define motorLeftForward    PINB4   /* Left motor, pin 2                */
+#define motorLeftBackward   PINB5   /* Left motor, pin 1                */
 
 /* Motor encoder                                                        */
 #define encoderPinLeft  PIND3       /*   Left encoder connected to PD3  */
@@ -88,9 +97,9 @@
 
 
 /* Servo                                                                */
-#define servoReg        DDRB        /* Port D register used             */
-#define servoPin        PINB5       /* Servo connected to pin 4         */
-#define servoOCR        OCR1A       /* Output compare register for PB5  */
+#define servoReg        DDRH        /* Port H register used             */
+#define servoPin        PINH4       /* Servo connected to pin H4         */
+#define servoOCR        OCR4B       /* Output compare register for PH4  */
 
 /* LED                                                                  */
 #define ledPORT		    PORTL   /*  LEDs connected to port D            */
@@ -100,12 +109,10 @@
 #define ledRED		    PINL0   /*  Green LED connected                 */
 
 /* Communication                                                        */
-#define USART_BAUDRATE 38400UL  /* If changed, also change in nRF51 code*/
-#define nRF17 PINA7             /* Available GPIO pins from nRF51 Dongle*/
-#define nRF18 PINC6             
+#define USART_BAUDRATE 38400UL  /* If changed, also change in nRF51 code*/       
 #define nRF19 PINE4             
 #define nRF51_status INT4_vect  /* PinE4 has interrupt available        */
-#define nRF20 PINC2             
+#define nRF20 PINE5				/* Unused at this time 					*/             
 
 #define DDR_SPI DDRB            /* Data direction register for SPI      */
 #define DD_MOSI PINB2           /* MOSI pin                             */
